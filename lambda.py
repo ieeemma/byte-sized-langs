b815b32c2ffa693041114ed9fc2f34f24d2c95a2
ieeemma <emma.freeman@protonmail.com>
2022-08-05 23:06:32 +0100

    Remove some extra characters

--- lambda.py
+++ lambda.py
@@ -1,6 +1,6 @@
 t=[*input()]
 z=t.pop
-E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t and t[0]==")"else[x,A()])(A())
+E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t[0]==")"else[x,A()])(A())
 A=lambda:(m:=z(0))and([E(),z(0)][0]if m=="("else m)
 Z=lambda*_,**k:lambda x,e=0:eval(k[str(type(x))[8]])
 F=Z(t='f"(λ{x[0]}.{F(x[1])})"',l='"("+F(x[0])+F(x[1])+")"',s='x')
