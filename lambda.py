82e6c40cec2e3b5ee6c7eae841f73bdd82fc0c35
ieeemma <emma.freeman@protonmail.com>
2022-08-05 22:28:38 +0100

    Switch lambda to string-eval based type dispatch

--- lambda.py
+++ lambda.py
@@ -1,11 +1,10 @@
 t=[*input()]
 z=t.pop
-T=type
-class Q(tuple):...
-E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t and t[0]==")"else Q([x,A()]))(A())
-A=lambda:(m:=z(0))and([E(),z(0)][0]if m=="("else m)
-F=lambda x,p:f"(λ{x[0]}.{F(x[1],0)})"if T(x)==tuple else((o:=F(x[0],0)+" "+F(x[1],1))and[o,f"({o})"][p])if T(x)==Q else x
-R=lambda x,e:(f:=R(x[0],e))and R(f[1],e|{f[0]:R(x[1],e)})if T(x)==Q else e.get(x,x)
+E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t and t[0]==")"else[x,A()])(A())
+A=lambda:(m:=z(0),E(),z(0))[1]if m=="("else m)
+Z=lambda*_,**k:lambda x,*y:eval(k[str(type(x))[8]])
+F=Z(t='f"(λ{x[0]}.{F(x[1],0)})"',l='(o:=F(x[0],0)+F(x[1],1))and[o,f"({o})"][y[0]]',s='x')
+R=Z(l='(e:=y[0],f:=R(x[0],e),R(f[1],e|{f[0]:R(x[1],e)}))[2]',t="x",s="y[0][x]")
 x=E()
-while (y:=R(x,{}))!=x:x=y
+while(y:=R(x,{}))!=x:x=y
 print(F(x,0))
