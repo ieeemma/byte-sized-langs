1ba728d96f42adb84a38dbefb131725f4da2af3d
ieeemma <emma.freeman@protonmail.com>
2022-08-05 22:04:33 +0100

    Switch lambda application type

--- lambda.py
+++ lambda.py
@@ -1,13 +1,39 @@
-t=__import__('re').findall(r'[().λ]|[a-z]+',"(λx. x)(λx. x)")
+i=__import__
+t=i('re').findall(r'[().λ]|[a-z]+',i("sys").stdin.read())
 z=t.pop
 T=type
-E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t and t[0]==")"else[x,A()])(A())
+class Q(tuple):...
+E=lambda:([z(0),z(0),z(0)][1],E())if t[0]=="λ"else(lambda x:x if t and t[0]==")"else Q([x,A()]))(A())
 A=lambda:(m:=z(0))and([E(),z(0)][0]if m=="("else m)
-R=lambda x,e:R(x[0][1],e|{x[0][0]:x[1]})if type(x)==list else e.get(x,x)
-F=lambda x,p:f"(λ{x[0]}. {F(x[1],0)})"if T(x)==tuple else((o:=F(x[0],0)+" "+F(x[1],1))and[o,f"({o})"][p])if T(x)==list else x
+F=lambda x,p:f"(λ{x[0]}. {F(x[1],0)})"if T(x)==tuple else((o:=F(x[0],0)+" "+F(x[1],1))and[o,f"({o})"][p])if T(x)==Q else x
+R=lambda x,e:(f:=R(x[0],e))and R(f[1],e|{f[0]:R(x[1],e)})if T(x)==Q else e.get(x,x)
 x=E()
 while (y:=R(x,{}))!=x:x=y
 print(F(x,0))
+
+
+
+# N = 0
+
+# def R(x,e):
+#   global N
+#   # if N==500:exit()
+#   print(" "*2*N + "eval " + F(x,0) + " under " + repr({k:F(v,1) for k,v in e.items()}))
+#   N+=1
+#   if T(x)==Q:
+#     f=R(x[0],e)
+#     # print(F(x[0],0), F(f,0), e)
+#     o=R(f[1], e|{f[0]:R(x[1],e)})
+#   else:o=e.get(x,x)
+
+#   N-=1
+#   print(" "*2*N + "=> " + F(o,0))
+#   return o
+
+# print(F(rx, 0))
+
+
+
 
 
 # def fmt(x, p):
