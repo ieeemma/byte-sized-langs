3288f4167bf66187a9e9580e35861e9278d633c1
ieeemma <emma.freeman@protonmail.com>
2022-08-06 14:05:02 +0100

    Add brainfuck interpreter

---
+++ brainfuck.py
@@ -0,0 +1,3 @@
+t=d=0;p,m="",[0]*1000
+for i in input():j="]+[-><.,".find(i);p+=" "*t+"0 m[d]+=1 while+m[d]: m[d]-=1 d+=1 d-=1 print(end=chr(m[d])) m[d]=ord(input())".split()[j]+"\n";t-=(j<3)*(1-j)
+exec(p)
